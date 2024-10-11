4ec418e3f510f571e73a64cbf5d0bc79f8e72523
Himanshu Jain <jainnhimanshuu@gmail.com>
2024-10-12 00:23:27 +0530

    first commit

---
+++ src/app/api/auth/instagram/callback/route.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+const CLIENT_ID = process.env.INSTAGRAM_CLIENT_ID;
+const CLIENT_SECRET = process.env.INSTAGRAM_CLIENT_SECRET;
+const REDIRECT_URI = process.env.INSTAGRAM_REDIRECT_URI;
+
+export async function GET(request) {
+  const { searchParams } = new URL(request.url);
+  const code = searchParams.get("code");
+
+  try {
+    const tokenResponse = await axios.post(
+      "https://api.instagram.com/oauth/access_token",
+      {
+        client_id: CLIENT_ID,
+        client_secret: CLIENT_SECRET,
+        grant_type: "authorization_code",
+        redirect_uri: REDIRECT_URI,
+        code: code,
+      }
+    );
+
+    const accessToken = tokenResponse.data.access_token;
+
+    // Fetch user info
+    const userInfoResponse = await axios.get(
+      `https://graph.instagram.com/me?fields=id,username&access_token=${accessToken}`
+    );
+    return NextResponse.json(userInfoResponse.data);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Error retrieving access token or user info" },
+      { status: 500 }
+    );
+  }
+}
---
+++ src/app/api/auth/instagram/route.js
@@ -0,0 +1,9 @@
+import { NextResponse } from "next/server";
+
+const CLIENT_ID = process.env.INSTAGRAM_CLIENT_ID;
+const REDIRECT_URI = process.env.INSTAGRAM_REDIRECT_URI;
+
+export async function GET() {
+  const url = `https://api.instagram.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=user_profile,user_media&response_type=code`;
+  return NextResponse.redirect(url);
+}
