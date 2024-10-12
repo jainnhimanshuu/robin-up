8e5b73a59045832910f0d60b02bbd1538635e319
Himanshu Jain <helpbeastprogramming@gmail.com>
2024-10-12 16:55:15 +0530

    Add: Instagram Insight

--- src/app/api/auth/instagram/callback/route.js
+++ src/app/api/auth/instagram/callback/route.js
@@ -29,9 +29,42 @@ export async function GET(request) {
 
     // Fetch user info
     const userInfoResponse = await axios.get(
-      `https://graph.instagram.com/me?fields=id,user_id,username&access_token=${accessToken}`
+      `https://graph.instagram.com/me?fields=id,user_id,username,name,account_type,profile_picture_url,followers_count,follows_count,media_count&access_token=${accessToken}`
     );
-    return NextResponse.json(userInfoResponse.data);
+
+    const userMediaObjectResponse = await axios.get(
+      `https://graph.instagram.com/v21.0/${userInfoResponse.data.user_id}/media?access_token=${accessToken}`
+    );
+
+    // Fetch media insights for each media item
+    const mediaInsightsPromises = userMediaObjectResponse.data.data.map(
+      (mediaItem) =>
+        axios
+          .get(
+            `https://graph.instagram.com/${mediaItem.id}/insights?metric=impressions,shares,comments,plays,likes,saved,replies,video_views,total_interactions,navigation,follows,profile_visits,profile_activity,reach,ig_reels_video_view_total_time,ig_reels_avg_watch_time,clips_replays_count,ig_reels_aggregated_all_plays_count,views,thread_replies,reposts,quotes,peak_concurrent_viewers,thread_shares&access_token=${accessToken}`
+          )
+          .catch((error) => {
+            console.error(
+              `Error fetching insights for media item ${mediaItem.id}`
+            );
+            return null;
+          })
+    );
+
+    const mediaInsightsResponses = await Promise.all(mediaInsightsPromises);
+    let mediaInsights = [];
+    if (
+      mediaInsightsResponses.filter((insight) => insight !== null).length > 0
+    ) {
+      mediaInsights = mediaInsightsResponses.map((response) => response.data);
+    }
+
+    const output = {
+      basic: userInfoResponse.data,
+      media: userMediaObjectResponse.data,
+      insights: mediaInsights,
+    };
+    return NextResponse.json(output);
   } catch (error) {
     console.error(error);
     return NextResponse.json(
