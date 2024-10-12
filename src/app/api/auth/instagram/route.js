dbac968fe41b7dca1201fbc68d6e49518b99d530
Himanshu Jain <helpbeastprogramming@gmail.com>
2024-10-12 10:21:50 +0530

    Scope Change

--- src/app/api/auth/instagram/route.js
+++ src/app/api/auth/instagram/route.js
@@ -4,6 +4,6 @@ const CLIENT_ID = process.env.INSTAGRAM_CLIENT_ID;
 const REDIRECT_URI = process.env.INSTAGRAM_REDIRECT_URI;
 
 export async function GET() {
-  const url = `https://api.instagram.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=user_profile,user_media&response_type=code`;
+  const url = `https://api.instagram.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=instagram_business_basic&response_type=code`;
   return NextResponse.redirect(url);
 }
