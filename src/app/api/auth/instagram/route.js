be47ae09164e00b570fbae2ada5e959ddd38a5d4
akhilesh-s <aksi9665@gmail.com>
2024-10-12 15:21:00 +0530

    aksi test

--- src/app/api/auth/instagram/route.js
+++ src/app/api/auth/instagram/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 const CLIENT_ID = process.env.INSTAGRAM_CLIENT_ID;
 const REDIRECT_URI = process.env.INSTAGRAM_REDIRECT_URI;
-
+console.log("test");
 export async function GET() {
   const url = `https://api.instagram.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=instagram_business_basic&response_type=code`;
   return NextResponse.redirect(url);
