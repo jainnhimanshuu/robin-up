ee4cfcb75ad19c74918549f4fd05aaac2dacbd92
Himanshu Jain <helpbeastprogramming@gmail.com>
2024-10-29 19:54:49 +0530

    [Page][Home] - Hero Section

---
+++ src/components/backgrounds/index.ts
@@ -0,0 +1,2 @@
+export * from "./BackgroundBeams";
+export * from "./AuroraBackground";
---
+++ src/constants/instausers.ts
@@ -0,0 +1,44 @@
+export const tooltipData = [
+  {
+    id: 0,
+    name: "Dnyaneshwari Bagekar",
+    username: "@dnyaneshwari_bagekar30",
+    image:
+      "https://instagram.fbom3-1.fna.fbcdn.net/v/t51.2885-19/395826892_785159500037828_8961014008698155994_n.jpg?_nc_ht=instagram.fbom3-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=zDqGlAsvHeUQ7kNvgGKK428&_nc_gid=38ff51db23074d5e9479ad9c61ad939f&edm=AP4sbd4BAAAA&ccb=7-5&oh=00_AYBOAd6G1MMRSP6WdaZohMMlUdksR3xte44DC-VOpELS3g&oe=67117FA5&_nc_sid=7a9f4b",
+  },
+  {
+    id: 1,
+    name: "Himanshu Jain",
+    username: "@himansh47",
+    image:
+      "https://instagram.fbom3-1.fna.fbcdn.net/v/t51.2885-19/461809134_850490693868918_533252623569482231_n.jpg?_nc_ht=instagram.fbom3-1.fna.fbcdn.net&_nc_cat=106&_nc_ohc=PBz9jOvs_bAQ7kNvgFQE0CB&_nc_gid=0f1a27436a2a4b8389a78475e0627a38&edm=AP4sbd4BAAAA&ccb=7-5&oh=00_AYDqRdERUNRHqw5wOrHuK9nfjjq8A4Qnt3mVHxLnrmqZkw&oe=67116AEC&_nc_sid=7a9f4b",
+  },
+  {
+    id: 2,
+    name: "Akhilesh Singh",
+    username: "@s__akhilesh",
+    image:
+      "https://instagram.fbom3-1.fna.fbcdn.net/v/t51.2885-19/457744786_483773731097970_2594624856962045227_n.jpg?_nc_ht=instagram.fbom3-1.fna.fbcdn.net&_nc_cat=106&_nc_ohc=Lys3PLWwIYkQ7kNvgEwuSXj&_nc_gid=923e6cace6364d3989e3da87145e335a&edm=AP4sbd4BAAAA&ccb=7-5&oh=00_AYBaLggBcEHyakaiwv-Hhd-t1znIGcJ4DIsJ-GfZ-2q_Tg&oe=6711B7D7&_nc_sid=7a9f4b",
+  },
+  {
+    id: 3,
+    name: "Mayuri Tanpatil",
+    username: "@mayu_tanpatil",
+    image:
+      "https://instagram.fbom3-2.fna.fbcdn.net/v/t51.2885-19/431176236_927864325405337_2967014259325318044_n.jpg?_nc_ht=instagram.fbom3-2.fna.fbcdn.net&_nc_cat=105&_nc_ohc=GKAQB2dtvV0Q7kNvgFqg9CY&_nc_gid=d7be58f14c154261a63b024de60a4bc9&edm=APoiHPcBAAAA&ccb=7-5&oh=00_AYDmX0rODvW0kjSCD7zbYoEVHILj9tAP1Oc5Fa2BHgp2EQ&oe=6711865A&_nc_sid=22de04",
+  },
+  {
+    id: 4,
+    name: "Ritesh Shetty",
+    username: "@shetty_ritesh",
+    image:
+      "https://instagram.fbom3-2.fna.fbcdn.net/v/t51.2885-19/57181877_2140383522914177_2079500689435787264_n.jpg?_nc_ht=instagram.fbom3-2.fna.fbcdn.net&_nc_cat=105&_nc_ohc=-02MpQ6KoU8Q7kNvgEgpq-b&_nc_gid=814655ca3a2440a0bd29971ce835d7e5&edm=AP4sbd4BAAAA&ccb=7-5&oh=00_AYAh_7pE1KTkBnxuzw9uuRk4PUcfnnU5glHwPjVpwQ8BAQ&oe=6711AB7F&_nc_sid=7a9f4b",
+  },
+  {
+    id: 5,
+    name: "Rohan Salunke",
+    username: "@ikuzo_rohan",
+    image:
+      "https://instagram.fbom3-2.fna.fbcdn.net/v/t51.2885-19/412643892_669559292011334_5272974791092008153_n.jpg?_nc_ht=instagram.fbom3-2.fna.fbcdn.net&_nc_cat=102&_nc_ohc=PZez25fM8XcQ7kNvgGCnBQr&_nc_gid=f605ea6c85234c0da0c5a4df54462e8a&edm=AP4sbd4BAAAA&ccb=7-5&oh=00_AYDTMcDR5GsZ2rwruOvGvnFAnj5Uge-lj8sNpIcQV51b4A&oe=6711A877&_nc_sid=7a9f4b",
+  },
+];
